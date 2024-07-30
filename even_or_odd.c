3f20e4d9e2f164ddaec6d8b7846926cb1d3bf87f
Lasantha Mudalige <lasantha1130@gmail.com>
2024-07-30 23:04:55 +0530

    add new odd even challenge

---
+++ even_or_odd.c
@@ -0,0 +1,32 @@
+/*
+Task
+
+ Create a function that takes an integer as an argument and returns "Even" for
+even numbers or "Odd" for odd numbers.
+*/
+
+#include <stdio.h>
+const char *even_or_odd(int);
+
+int main(void) {
+  /* Input: 0, 1, 21, 2, 100, -1, -21, -2, -100 */
+
+  /* Test numbers */
+  int numbers[9] = {0, 1, 21, 2, 100, -1, -21, -2, -100};
+
+  for (int i = 0; i < 9; i++) {
+    printf("%s ", even_or_odd(numbers[i]));
+  }
+
+  /* Output: Even Odd Odd Even Even Odd Odd Even Even */
+
+  return 0;
+}
+
+const char *even_or_odd(int number) {
+  if (number % 2 == 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
